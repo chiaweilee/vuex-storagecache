051478e7e73247b0bd65075255845240dfc2334a
jv <ljw@live.jp>
2018-07-18 14:03:37 +0800

    0.1.2 refactor https://github.com/chiaweilee/vue-calc/issues/1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
-const prefix = x => (() => x.toString().split('.')[1] || [])().length
+const fix = x => (x.toString().split('.')[1] || '').length
+const rtl = x => Number(x.toString().split('.').join(''))
 
 class Calculator {
   constructor (x) {
-    this.x = x
-    this.z = prefix(x) || 1
+    this.z = fix(x)
+    this.x = rtl(x)
   }
   prefix (y) {
-    const _z = prefix(y)
-    if (_z > this.z) this.z = _z
-    return Math.pow(10, this.z)
+    const z = fix(y)
+    if (z > this.z) {
+      this.x *= Math.pow(10, z - this.z)
+      this.y = rtl(y)
+      // set this.z at the end
+      this.z = z
+      return
+    } else if (z < this.z) {
+      this.y = rtl(y) * Math.pow(10, this.z - z)
+      return
+    }
+    this.y = rtl(y)
   }
   add (y) {
-    const z = this.prefix(y)
-    return (this.x * z + y * z) / z
+    this.prefix(y)
+    return (this.x + this.y) / Math.pow(10, this.z)
   }
   subtract (y) {
     return this.add(-y)
   }
   multiply (y) {
-    const z = this.prefix(y)
-    return ((this.x * z) * (y * z)) / (z * z)
+    this.prefix(y)
+    return (this.x * this.y) / Math.pow(Math.pow(10, this.z), 2)
   }
   divide (y) {
-    const z = this.prefix(y)
-    return (this.x * z) / (y * z)
+    this.prefix(y)
+    return (this.x / this.y)
   }
 }
 
