c1845db1eb369d48113040864bff2bfa07f6057e
jv <ljw@live.jp>
2018-09-21 04:16:39 +0800

    0.1.4

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ const rtl = x => Number(x.toString().split('.').join(''))
 
 class Calculator {
   constructor (x) {
+    if (x === undefined) {
+      console.warn('$calc(x), x is undefined.')
+      return
+    }
     this.z = fix(x)
     this.x = rtl(x)
   }
   prefix (y) {
+    if (x === undefined) {
+      console.warn('prefix(x), x is undefined.')
+      return
+    }
     const z = fix(y)
     if (z > this.z) {
       this.x *= Math.pow(10, z - this.z)
@@ -21,17 +29,33 @@ class Calculator {
     this.y = rtl(y)
   }
   add (y) {
+    if (x === undefined) {
+      console.warn('.add(x), x is undefined.')
+      return
+    }
     this.prefix(y)
     return (this.x + this.y) / Math.pow(10, this.z)
   }
   subtract (y) {
+    if (x === undefined) {
+      console.warn('.subtract(x), x is undefined.')
+      return
+    }
     return this.add(-y)
   }
   multiply (y) {
+    if (x === undefined) {
+      console.warn('.multiply(x), x is undefined.')
+      return
+    }
     this.prefix(y)
     return (this.x * this.y) / Math.pow(Math.pow(10, this.z), 2)
   }
   divide (y) {
+    if (x === undefined) {
+      console.warn('.divide(x), x is undefined.')
+      return
+    }
     this.prefix(y)
     return (this.x / this.y)
   }
