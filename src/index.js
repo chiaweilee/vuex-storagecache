be6444af9fbba03d328e760dc2f3fd6ff5bdb84d
jv <ljw@live.jp>
2018-09-27 10:54:16 +0800

    0.0.1

--- src/index.js
+++ src/index.js
@@ -1,70 +1,66 @@
-const fix = x => (x.toString().split('.')[1] || '').length
-const rtl = x => Number(x.toString().split('.').join(''))
-
-class Calculator {
-  constructor (x) {
-    if (x === undefined) {
-      console.warn('$calc(x), x is undefined.')
-      return
-    }
-    this.z = fix(x)
-    this.x = rtl(x)
-  }
-  prefix (y) {
-    if (x === undefined) {
-      console.warn('prefix(x), x is undefined.')
-      return
-    }
-    const z = fix(y)
-    if (z > this.z) {
-      this.x *= Math.pow(10, z - this.z)
-      this.y = rtl(y)
-      // set this.z at the end
-      this.z = z
-      return
-    } else if (z < this.z) {
-      this.y = rtl(y) * Math.pow(10, this.z - z)
-      return
-    }
-    this.y = rtl(y)
-  }
-  add (y) {
-    if (x === undefined) {
-      console.warn('.add(x), x is undefined.')
-      return
-    }
-    this.prefix(y)
-    return (this.x + this.y) / Math.pow(10, this.z)
-  }
-  subtract (y) {
-    if (x === undefined) {
-      console.warn('.subtract(x), x is undefined.')
-      return
-    }
-    return this.add(-y)
-  }
-  multiply (y) {
-    if (x === undefined) {
-      console.warn('.multiply(x), x is undefined.')
-      return
-    }
-    this.prefix(y)
-    return (this.x * this.y) / Math.pow(Math.pow(10, this.z), 2)
-  }
-  divide (y) {
-    if (x === undefined) {
-      console.warn('.divide(x), x is undefined.')
-      return
-    }
-    this.prefix(y)
-    return (this.x / this.y)
+const set = (name, data, useSession) => (useSession ? sessionStorage : localStorage).setItem(name, JSON.stringify(data))
+const get = (name, useSession) => {
+  try {
+    return JSON.parse((useSession ? sessionStorage : localStorage).getItem(name)) || {}
+  } catch (err) {
+    console.error(err)
+    return {}
   }
 }
 
-const calc = x => new Calculator(x)
-
-const install = Vue => { Object.defineProperty(Vue.prototype, '$calc', {value: calc}) }
-
-export default {
-  install
+export default option => store => {
+  // initialized
+  const xName = option.name || 'x_stateCache'
+  const { cacheState, sessionState } = option
+  const { state, _vm } = store
+  const { $set } = _vm
+  const stateCache = get(xName)
+  const sessionCache = get(xName, true)
+  // fn
+  const climb = (cacheName, writeValue, useSession) => {
+    let value = state
+    cacheName = cacheName.split('.')
+    const lastIndex = cacheName.length - 1
+    cacheName.forEach((name, index) => {
+      if (writeValue && lastIndex === index) {
+        const cache = useSession ? sessionCache : stateCache
+        if (!!cache.name && Object.keys(cache.name).length) {
+          console.log(value)
+          console.log(name)
+          console.log(cache.name)
+          $set(value, name, cache.name)
+        }
+        return
+      }
+      value = value[name]
+    })
+    return value
+  }
+  const watch = (cacheName, useSession) => {
+    store.watch(
+      store => climb(cacheName),
+      to => {
+        const cache = get(xName, !!useSession)
+        cache[cacheName] = to
+        set(xName, cache, !!useSession)
+      },
+      {
+        deep: true
+      }
+    )
+  }
+  // check
+  if (!(cacheState instanceof Array)) {
+    console.warn('cacheState should be an Array.')
+    return
+  }
+  //
+  cacheState.forEach(cacheName => {
+    climb(cacheName, true)
+    watch(cacheName)
+  })
+  sessionState.forEach(cacheName => {
+    climb(cacheName, true, true)
+    watch(cacheName, true)
+  })
 }
